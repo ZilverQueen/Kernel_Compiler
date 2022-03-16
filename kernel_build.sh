84f965e645e1ad65fb4485f3cb4ec5311155029f
ZilverQueen <89440054+ZilverQueen@users.noreply.github.com>
2022-03-16 18:33:35 +0700

    upstreamed

--- kernel_build.sh
+++ kernel_build.sh
@@ -17,14 +17,14 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth=1 https://github.com/MhmmdAmier/kernel_xiaomi_ulysse-4.9.git -b a12/temp ulysse
-git clone --depth=1 https://github.com/amier2002/GabutersTC-clang GabutersTC # xRageTC set as Clang Default
+git clone --depth=1 https://github.com/mgs28-mh/kernel_xiaomi_ulysse-4.9.git -b a12/temp ulysse
+git clone --depth=1 https://github.com/Gabuters-Dev/gabuters-clang GABUTERSxTC
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/ulysse # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_DEFCONFIG=ulysse_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=$(pwd)/GabutersTC # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER=amier_san09 # Change with your own name or else.
+CLANG_ROOTDIR=$(pwd)/GABUTERSxTC # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=nobody # Change with your own name or else.
 export KBUILD_BUILD_HOST=Gabuters-dev # Change with your own hostname.
 
 # Main Declaration
